f655ccf68331961df0dfafde1a107e502ca42cf5
Florian Strunz <florian.strunz@stud-mail.uni-wuerzburg.de>
2021-10-28 19:22:13 +0200

    Move metadata test cases into unit test itself

--- page/test/test_parse_metadata.py
+++ page/test/test_parse_metadata.py
@@ -1,59 +1,89 @@
 import unittest
-from page import Page
 from page.exceptions import PageXMLError
+from page.elements.metadata import Metadata
+from lxml import etree
 
-EXAMPLE_SIMPLE = "page/test/files/metadata/simple.xml"
-EXAMPLE_NO_COMMENT = "page/test/files/metadata/no_comment.xml"
-EXAMPLE_MALFORMED_DATE = "page/test/files/metadata/malformed_date.xml"
-EXAMPLE_EMPTY_CREATOR = "page/test/files/metadata/empty_creator.xml"
-EXAMPLE_EMPTY_COMMENT = "page/test/files/metadata/empty_comment.xml"
+SIMPLE_METADATA = etree.XML(
+    """<Metadata>
+        <Creator>Test Creator</Creator>
+        <Created>2021-10-21T18:37:36</Created>
+        <LastChange>1970-01-01T00:00:00</LastChange>
+        <Comments>Test Comment</Comments>
+    </Metadata>"""
+)
+
+NO_COMMENT_METADATA = etree.XML(
+    """<Metadata>
+        <Creator>Test Creator</Creator>
+        <Created>2021-10-21T18:37:36</Created>
+        <LastChange>1970-01-01T00:00:00</LastChange>
+    </Metadata>"""
+)
+
+MALFORMED_DATE_METADATA = etree.XML(
+    """<Metadata>
+        <Creator>Test Creator</Creator>
+        <Created>invalid date</Created>
+        <LastChange>ᓰᘉᐺᗩᒪᓰᕲ ᕲᗩᖶᘿ</LastChange>
+        <Comments>Test Comment</Comments>
+    </Metadata>"""
+)
+
+EMPTY_CREATOR_METADATA = etree.XML(
+    """<Metadata>
+        <Creator/>
+        <Created>2021-10-21T18:37:36</Created>
+        <LastChange>1970-01-01T00:00:00</LastChange>
+        <Comments>Test Comment</Comments>
+    </Metadata>"""
+)
+
+EMPTY_COMMENT_METADATA = etree.XML(
+    """<Metadata>
+        <Creator>Test Creator</Creator>
+        <Created>2021-10-21T18:37:36</Created>
+        <LastChange>1970-01-01T00:00:00</LastChange>
+        <Comments></Comments>
+    </Metadata>
+    """
+)
 
 
 class TestParseMetadata(unittest.TestCase):
     def test_simple(self):
-        with open(EXAMPLE_SIMPLE, "r") as file:
-            page = Page.from_file(file)
-            meta = page.metadata
+        meta = Metadata.from_element(SIMPLE_METADATA, {})
 
-            self.assertEqual(meta.creator, "Test Creator")
-            self.assertEqual(meta.comments, "Test Comment")
+        self.assertEqual(meta.creator, "Test Creator")
+        self.assertEqual(meta.comments, "Test Comment")
 
-            created = (
-                meta.created.year,
-                meta.created.month,
-                meta.created.day
-            )
-            self.assertEqual(created, (2021, 10, 21))
+        created = (
+            meta.created.year,
+            meta.created.month,
+            meta.created.day
+        )
+        self.assertEqual(created, (2021, 10, 21))
 
-            last_change = (
-                meta.last_change.year,
-                meta.last_change.month,
-                meta.last_change.day
-            )
-            self.assertEqual(last_change, (1970, 1, 1))
+        last_change = (
+            meta.last_change.year,
+            meta.last_change.month,
+            meta.last_change.day
+        )
+        self.assertEqual(last_change, (1970, 1, 1))
 
     def test_empty_creator(self):
-        with open(EXAMPLE_EMPTY_CREATOR, "r") as file:
-            page = Page.from_file(file)
-            meta = page.metadata
-            self.assertEqual(meta.creator, "")
+        meta = Metadata.from_element(EMPTY_CREATOR_METADATA, {})
+        self.assertEqual(meta.creator, "")
 
     def test_no_comment(self):
-        with open(EXAMPLE_NO_COMMENT, "r") as file:
-            page = Page.from_file(file)
-            meta = page.metadata
-            self.assertEqual(meta.comments, None)
+        meta = Metadata.from_element(NO_COMMENT_METADATA, {})
+        self.assertEqual(meta.comments, None)
 
     def test_empty_comment(self):
-        with open(EXAMPLE_EMPTY_COMMENT, "r") as file:
-            page = Page.from_file(file)
-            meta = page.metadata
-            self.assertEqual(meta.comments, "")
+        meta = Metadata.from_element(EMPTY_COMMENT_METADATA, {})
+        self.assertEqual(meta.comments, "")
 
     def test_malformed_date(self):
-        with open(EXAMPLE_MALFORMED_DATE, "r") as file:
-            self.assertRaises(PageXMLError, lambda: Page.from_file(file))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertRaises(
+            PageXMLError,
+            lambda: Metadata.from_element(MALFORMED_DATE_METADATA, {})
+        )
