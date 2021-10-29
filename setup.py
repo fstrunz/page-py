98cc182a2d35a314c094a5c599ec9c17d122b157
Florian Strunz <florian.strunz@stud-mail.uni-wuerzburg.de>
2021-10-29 10:25:17 +0200

    Use setuptools properly in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='page-py',
     version='1.0',
     description='Library for dealing with PAGE XML files.',
     install_requires=['lxml>=4.6.0', 'python-dateutil>=2.8.0'],
+    tests_require=['rstr>=3.0.0'],
+    test_suite='page.test',
     packages=['page']
 )
